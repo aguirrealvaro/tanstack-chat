19166b94d0c3d4fd04399e67744b6574a9620823
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2026-05-08 19:30:06 -0300

    add header user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import HeaderUser from "#/integrations/clerk/header-user";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({ component: Home });
@@ -7,8 +8,9 @@ function Home() {
     <div className="p-8">
       <h1 className="text-4xl font-bold">Welcome to TanStack Start</h1>
       <p className="mt-4 text-lg">
-        Edit <code>src/routes/index.tsx</code> to get started.
+        Edit <code>src/routes/index.tsx</code> to get started
       </p>
+      <HeaderUser />
     </div>
   );
 }
