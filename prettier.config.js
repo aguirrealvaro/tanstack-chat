e9577431edd179bc236364b11bbaeb4e75e607ff
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2026-05-08 17:52:48 -0300

    add prettier-plugin-tailwind

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,6 @@
 //  @ts-check
 
-/** @type {import('prettier').Config} */
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   semi: true,
   singleQuote: false,
@@ -8,6 +8,8 @@ const config = {
   printWidth: 95,
   arrowParens: "always",
   endOfLine: "lf",
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindStylesheet: "./src/styles.css",
 };
 
 export default config;
