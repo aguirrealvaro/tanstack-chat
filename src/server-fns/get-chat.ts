0309bb26e2445f7ad213cb1d6740648074991c8c
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2026-05-12 18:10:41 -0300

    remove sleeps

--- src/server-fns/get-chat.ts
+++ src/server-fns/get-chat.ts
@@ -2,13 +2,13 @@ import { prisma } from "@/db";
 import { createServerFn } from "@tanstack/react-start";
 import { z } from "zod";
 import { authMiddleware } from "./auth-middleware";
-import { sleep } from "@/lib/sleep";
+// import { sleep } from "@/lib/sleep";
 
 export const getChat = createServerFn()
   .middleware([authMiddleware])
   .inputValidator(z.number())
   .handler(async ({ data: userSelected, context: { currentUser } }) => {
-    await sleep(5000);
+    // await sleep(5000);
     await prisma.message.updateMany({
       where: { fromId: Number(userSelected), toId: currentUser.id },
       data: { seen: true },
