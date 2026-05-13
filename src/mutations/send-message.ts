1e7ba60d776cb2a7d78271b5ce023f12fd7b7f81
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2026-05-13 20:57:23 -0300

    clean mutation

--- src/mutations/send-message.ts
+++ src/mutations/send-message.ts
@@ -11,32 +11,37 @@ type SendMessageMutation = {
 export const useSendMessageMutation = () => {
   const queryClient = useQueryClient();
   const { currentUser } = HomeRoute.useRouteContext();
+
+  const optimisticUpdateMessages = async (data: SendMessageMutation) => {
+    // Cancel any outgoing refetches
+    await queryClient.cancelQueries({ queryKey: ["chat", data.selectedUser] });
+
+    const previousMessages = queryClient.getQueryData<Message[]>(["chat", data.selectedUser]);
+
+    const messageToAdd = {
+      id: 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      text: data.message,
+      fromId: currentUser.id,
+      toId: data.selectedUser,
+      seen: false,
+    };
+
+    const newMessages = previousMessages
+      ? [...previousMessages, messageToAdd]
+      : [messageToAdd];
+
+    queryClient.setQueryData(["chat", data.selectedUser], newMessages);
+
+    return { previousMessages };
+  };
+
   return useMutation({
     mutationFn: ({ message, selectedUser: toUserId }: SendMessageMutation) =>
       sendMessage({ data: { message, selectedUser: toUserId } }),
     onMutate: async (data) => {
-      // Cancel any outgoing refetches
-      await queryClient.cancelQueries({ queryKey: ["chat", data.selectedUser] });
-
-      const previousMessages = queryClient.getQueryData<Message[]>([
-        "chat",
-        data.selectedUser,
-      ]);
-
-      const newMessage: Message = {
-        id: 0,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        text: data.message,
-        fromId: currentUser.id,
-        toId: data.selectedUser,
-        seen: false,
-      };
-
-      const newMessages = previousMessages ? [...previousMessages, newMessage] : [newMessage];
-
-      queryClient.setQueryData(["chat", data.selectedUser], newMessages);
-
+      const { previousMessages } = await optimisticUpdateMessages(data);
       return { previousMessages };
     },
     onError: (_error, data, context) => {
