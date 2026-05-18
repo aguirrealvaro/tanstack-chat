4da3a3b0cc04988b1e856091d3bc3c8d57f0fe4a
Alvaro Aguirre <1aguirrealvaro@gmail.com>
2026-05-18 15:37:57 -0300

    remove uneed auth validation

--- src/routes/_auth.tsx
+++ src/routes/_auth.tsx
@@ -1,19 +1,6 @@
-import { auth } from "@clerk/tanstack-react-start/server";
-import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
-
-const getAuthState = createServerFn().handler(async () => {
-  const { userId } = await auth();
-  return { userId };
-});
+import { Outlet, createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_auth")({
-  beforeLoad: async () => {
-    const { userId } = await getAuthState();
-    if (userId) {
-      throw redirect({ to: "/", search: { user: undefined } });
-    }
-  },
   component: AuthLayout,
 });
 
